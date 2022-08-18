0565a29a034a90a97aea2e626229a66a773b7666
N.J. Childs <sirbeerus@gmail.com>
2022-08-17 21:35:01 -0400

    17/8/2022

--- PHP/Laravel/Sites/personal/app/Console/Commands/RatsDAOnft.php
+++ PHP/Laravel/Sites/personal/app/Console/Commands/RatsDAOnft.php
@@ -27,20 +27,23 @@ class RatsDAOnft extends Command
      * @return int
      */
     public function handle()
+
     {
-        $response = Http::get('https://api.opencnft.io/1/policy/ca5fc915496a771109b98c4a2b76e32c21a8229f3332398cb8babcd7/floor_price')->json();
 
-$floor = $response['floor_price'] / 1000000;
+    $response = Http::get('https://api.opencnft.io/1/policy/ca5fc915496a771109b98c4a2b76e32c21a8229f3332398cb8babcd7/floor_price')->json();
 
-if ($floor > 100) {
-    $phrase = 'RatsDAOnft floor price is: ' . $floor;
+    $floor = $response['floor_price'] / 1000000;
+    
+    if ( $floor < 25 || $floor > 200 ) {
+        $phrase = 'RatsDAOnft floor price is: ' . $floor;
+    
+        $cmd = sprintf(
+            'osascript -e \'display notification "%s" with title "ratsNFT Floor has moved!"\'',
+            $phrase
+        );
+    
+        shell_exec($cmd);
+    }
 
-    $cmd = sprintf(
-        'osascript -e \'display notification "%s" with title "Rats Floor has moved up!"\'',
-        $phrase
-    );
-
-    shell_exec($cmd);
-}
-}
+    }
 }
